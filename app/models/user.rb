26c025b95d4daf6449510e31733e94703f4bb6a7
suelenfeijo <suelenfeijobr@gmail.com>
2024-02-06 19:00:06 -0300

    ajustando métodos para usar do devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # se errar senha algumas vezes bloqueia :lockable, :timeoutable, and autenticação usando face, etc :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable, 
+         :validatable,
+         :confirmable,
+         :trackable
 end
